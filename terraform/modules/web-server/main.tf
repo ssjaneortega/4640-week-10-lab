2f528dbc69e167c5409c01a8d30f29802152b3ad
user <user@Ubuntu.myguest.virtualbox.org>
2025-03-14 14:56:06 -0700

    main.tf edit

--- terraform/modules/web-server/main.tf
+++ terraform/modules/web-server/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
 	ami = var.ami
 	instance_type = var.instance_type
 	key_name = var.key_name
-	vpc_security_group_ids = var.vpc_security_groups_ids
+	vpc_security_group_ids = var.vpc_security_group_ids
 	subnet_id = var.subnet_id
 
 	tags = {
