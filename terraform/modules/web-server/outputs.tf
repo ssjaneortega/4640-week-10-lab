5dc9b0954625552b6f1528a8d3a3cc7cf400eab4
user <user@Ubuntu.myguest.virtualbox.org>
2025-03-14 15:03:41 -0700

    yes

--- terraform/modules/web-server/outputs.tf
+++ terraform/modules/web-server/outputs.tf
@@ -1,8 +1,8 @@
-output "instance_ip_address" {
+output "instance_ip" {
 	value = aws_instance.web.public_ip
 }
 
-output "instance_dns_name" {
+output "instance_dns" {
 	value = aws_instance.web.public_dns
 }
 
